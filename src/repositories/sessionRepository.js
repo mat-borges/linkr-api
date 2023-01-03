7528e07db4b797846a100e9e8d2940e8aedae8f8
Tadeuvelloso <tadeuvellososilva@gmail.com>
2023-01-03 20:01:31 -0300

    feat: closesession query in sessionrepository

--- src/repositories/sessionRepository.js
+++ src/repositories/sessionRepository.js
@@ -4,8 +4,14 @@ async function newSession (user_id, token) {
     return connection.query(`INSERT INTO sessions (user_id, token) VALUES ($1, $2);`, [user_id, token]);
 }
 
+async function closeSession (token) {
+    return connection.query(`UPDATE sessions SET status=false WHERE token=$1;`, [token]);
+}
+
+
 const sessionRepository = {
-    newSession
+    newSession,
+    closeSession
 };
 
 export default sessionRepository;
