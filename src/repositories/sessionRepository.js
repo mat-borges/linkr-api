f7a0d3aafe84e3f3b747dd209709c0c2a0060cbc
Tadeuvelloso <tadeuvellososilva@gmail.com>
2023-01-03 20:04:47 -0300

    feat: getsession query func. in sessionrepository

--- src/repositories/sessionRepository.js
+++ src/repositories/sessionRepository.js
@@ -2,16 +2,20 @@ import { connection } from "../db/db.js";
 
 async function newSession (user_id, token) {
     return connection.query(`INSERT INTO sessions (user_id, token) VALUES ($1, $2);`, [user_id, token]);
-}
+};
 
 async function closeSession (token) {
     return connection.query(`UPDATE sessions SET status=false WHERE token=$1;`, [token]);
-}
+};
 
+async function getSession (token) {
+    return connection.query(`SELECT * FROM sessions WHERE token=$1;`, [token]);
+};
 
 const sessionRepository = {
     newSession,
-    closeSession
+    closeSession,
+    getSession
 };
 
 export default sessionRepository;
