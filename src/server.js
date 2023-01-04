8007892e6eed4304fca68e4d83f5e0dc828cf760
Mateus Borges <borgesm.work@gmail.com>
2023-01-04 18:40:59 -0300

    fix: merge bug

--- src/server.js
+++ src/server.js
@@ -1,15 +1,20 @@
 import express, { json } from 'express';
+
+import authRoutes from './routes/authRouter.js';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import authRoutes from "./routes/auth.Routes.js";
+import postRouter from './routes/postsRouter.js';
+import { stripHtml } from 'string-strip-html';
+
+export const cleanStringData = (string) => stripHtml(JSON.stringify(string)?.replace(/"|"/gi, ``)).result.trim();
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(json());
-app.use(authRoutes);
 
+app.use(authRoutes);
 app.use(postRouter);
 
 const port = process.env.PORT;
