7a9156b24d4e6e20b39e7f8503617fa3a27949b2
Tadeuvelloso <tadeuvellososilva@gmail.com>
2023-01-04 20:24:40 -0300

    feat: controller to signin

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -1,5 +1,9 @@
 import sessionRepository from "../repositories/sessionRepository.js";
 import userRepository from "../repositories/userRepository.js";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export async function signUp (req, res) {
     const {name, email, password, image} = res.locals.auth;
@@ -10,4 +14,22 @@ export async function signUp (req, res) {
     } catch (error) {
         return res.status(500).send(error.message);
     }
+}
+export async function signIn (req, res) {
+     const user = res.locals.user;
+
+     try {
+        const userDb = (await userRepository.getUserByEmail(user.email)).rows[0];
+
+        const {name, image, id} = userDb;
+
+        const token = jwt.sign({ id, name, image }, process.env.SECRET);
+
+        await sessionRepository.newSession(id, token);
+        
+        res.send(token)
+
+     } catch (error) {
+        return res.status(500).send(error.message);
+     }
 }
