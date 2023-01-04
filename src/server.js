e6c18e2fc3fb9947470f5fc8d3700233ee867ec1
Tadeuvelloso <tadeuvellososilva@gmail.com>
2023-01-04 17:48:59 -0300

    feat: server connection with authRoutes

--- src/server.js
+++ src/server.js
@@ -1,13 +1,14 @@
 import express, { json } from 'express';
-
 import cors from 'cors';
 import dotenv from 'dotenv';
+import authRoutes from "./routes/auth.Routes.js";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(json());
+app.use(authRoutes);
 
 const port = process.env.PORT;
 app.listen(port, () => console.log(`Running server on http://localhost:${port}`));
