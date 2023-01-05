d3f7110554b76504a1fc6a705a6392cee840b98a
Tadeuvelloso <tadeuvellososilva@gmail.com>
2023-01-04 21:09:06 -0300

    fix: return to user object

--- src/middlewares/sessionMiddleware.js
+++ src/middlewares/sessionMiddleware.js
@@ -1,20 +1,27 @@
 import sessionRepository from "../repositories/sessionRepository.js";
 
-export async function checkUserSession (req, res, next) {
+export async function checkUserSession(req, res, next) {
     const user = res.locals.user;
 
     try {
         const sessionExist = (await sessionRepository.getSessionById(user.id)).rows[0];
-        
-        if(sessionExist){
-            res.send(sessionExist.token, user.image, user.name);
+       
+        if (sessionExist) {
+
+            const obj = {
+                token: sessionExist.token,
+                name: user.name,
+                image: user.image
+            }
+
+            res.send(obj);
             return
-        } 
-        
+        }
+
         res.locals.user = user;
         next();
-        
-    } catch(error) {
+
+    } catch (error) {
         return res.status(500).send(error.message);
     }
 }
