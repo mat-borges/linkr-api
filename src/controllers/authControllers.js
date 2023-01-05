d80993db49636388172abd414d0ad5cd0b656da1
Tadeuvelloso <tadeuvellososilva@gmail.com>
2023-01-04 21:06:39 -0300

    fix: return obj to user

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -5,8 +5,8 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export async function signUp (req, res) {
-    const {name, email, password, image} = res.locals.auth;
+export async function signUp(req, res) {
+    const { name, email, password, image } = res.locals.auth;
 
     try {
         await userRepository.newUser(name, email, password, image);
@@ -15,21 +15,27 @@ export async function signUp (req, res) {
         return res.status(500).send(error.message);
     }
 }
-export async function signIn (req, res) {
-     const user = res.locals.user;
+export async function signIn(req, res) {
+    const user = res.locals.user;
 
-     try {
+    try {
         const userDb = (await userRepository.getUserByEmail(user.email)).rows[0];
 
-        const {name, image, id} = userDb;
+        const { name, image, id } = userDb;
 
         const token = jwt.sign({ id, name, image }, process.env.SECRET);
 
         await sessionRepository.newSession(id, token);
-        
-        res.send(token)
 
-     } catch (error) {
+        const obj = {
+            token,
+            name,
+            image
+        }
+
+        res.send(obj)
+
+    } catch (error) {
         return res.status(500).send(error.message);
-     }
+    }
 }
