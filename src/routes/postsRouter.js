d9c01f1848aedf28abf5c6764a3c03ecec26e08d
Mateus Borges <borgesm.work@gmail.com>
2023-01-06 12:23:01 -0300

    feat: hashtag middlewares, posts controller and router complete

--- src/routes/postsRouter.js
+++ src/routes/postsRouter.js
@@ -1,9 +1,12 @@
+import { authenticateUser, verifySession } from '../middlewares/verifyAuthorizationMiddleware.js';
+
 import { Router } from 'express';
+import { hashtagExists } from '../middlewares/hashtagsMiddleware.js';
 import { postSchemaValidation } from '../middlewares/postsMiddleware.js';
 import { publishLink } from '../controllers/postsController.js';
 
 const router = Router();
 
-router.post('/posts/publish', postSchemaValidation, publishLink);
+router.post('/posts/publish', authenticateUser, verifySession, postSchemaValidation, hashtagExists, publishLink);
 
 export default router;
