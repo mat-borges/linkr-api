e9c1126dea502161af1643c30262aa47b0e93e90
Oscar Maia <oscarfgmaia@gmail.com>
2023-01-07 14:56:59 -0300

    Revert "refact: name changed to singular singular instead of plural"
    This reverts commit 6462d87159dabc01273faaddfa85fbd4dd4b0379.

--- src/routes/hashtagRouter.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { showPostsRelatedToHashtag } from "../controllers/trendController.js";
-
-const router = Router();
-
-router.get("/hashtag/:hashtag", showPostsRelatedToHashtag);
-
-export default router;
