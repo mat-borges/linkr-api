9b815d6f7062453f32cce2e45dd4aa88df9f59a4
Oscar Maia <oscarfgmaia@gmail.com>
2023-01-07 15:09:30 -0300

    feat: send status 404 if there's no post.

--- src/controllers/trendController.js
+++ src/controllers/trendController.js
@@ -4,6 +4,9 @@ export async function showPostsRelatedToHashtag(req, res) {
     const hashtag = req.params.hashtag;
   try {
     const posts = await hashtagsRepository.getPostsRelatedToHashtag(hashtag);
+    if(posts.rowCount === 0){
+      return res.sendStatus(404);
+    }
     res.send(posts.rows);
   } catch (err) {
     console.log(err);
