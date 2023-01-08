1cb6f82ef47c5a8cff39a9e6d2bda3f3c42c50fc
Tadeuvelloso <tadeuvellososilva@gmail.com>
2023-01-08 20:56:35 -0300

    feat: logOut Controler & route

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { signUp, signIn } from "../controllers/authControllers.js";
+import { signUp, signIn, logOut } from "../controllers/authControllers.js";
 import { authBodyValidation, checkEmailInDb, checkObjToSignIn, checkObjSchema } from "../middlewares/authMiddleware.js";
 import { checkUserSession } from "../middlewares/sessionMiddleware.js";
 
@@ -7,5 +7,6 @@ const router = Router();
 
 router.post("/signup", authBodyValidation, checkEmailInDb, signUp);
 router.post("/signin", checkObjSchema, checkObjToSignIn, checkUserSession, signIn);
+router.post("/logout", logOut)
 
 export default router;
