9f4e5119f2e5f34c39fc528cbfef43723e102c6a
Mateus Borges <borgesm.work@gmail.com>
2023-01-08 18:34:53 -0300

    feat: search users (route, controller, repository)

---
+++ src/controllers/searchControllers.js
@@ -0,0 +1,22 @@
+import userRepository from '../repositories/userRepository.js';
+
+export async function search(req, res) {
+  const { input } = req.body;
+  let result = [];
+
+  try {
+    if (input.includes('#')) {
+      const hashtag = input.replace('#', '');
+      console.log({ hashtag });
+      result = { hashtag };
+    } else {
+      const name = `${input}%`;
+      const users = (await userRepository.searchUser(name)).rows;
+      result = users;
+    }
+    res.send(result);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+}
