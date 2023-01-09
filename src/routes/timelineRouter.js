fd8c3cce684b223acf3fb1d941a4cf6edd8d7b05
Mateus Borges <borgesm.work@gmail.com>
2023-01-09 14:46:17 -0300

    feat: get posts by user id, timeline/user/:id route fully functional

--- src/routes/timelineRouter.js
+++ src/routes/timelineRouter.js
@@ -1,11 +1,13 @@
+import { authenticateUser, verifySession } from '../middlewares/verifyAuthorizationMiddleware.js';
+import { showPosts, showPostsOfUser } from '../controllers/timelineController.js';
+
 import { Router } from 'express';
 import { deletePost } from '../controllers/postsController.js';
-import { showPosts } from '../controllers/timelineController.js';
-
 
 const router = Router();
 
 router.get('/timeline', showPosts);
-router.delete('/timeline/:id', deletePost)
+router.delete('/timeline/:id', deletePost);
+router.get('/timeline/user/:id', authenticateUser, verifySession, showPostsOfUser);
 
 export default router;
