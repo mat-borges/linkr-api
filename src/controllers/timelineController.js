57344e652f82aff13b7822888fbd3661bd94e254
Oscar Maia <oscarfgmaia@gmail.com>
2023-01-09 16:30:29 -0300

    feat: merge posts with metadatas in timeline

--- src/controllers/timelineController.js
+++ src/controllers/timelineController.js
@@ -1,9 +1,42 @@
-import  timelineRepository  from "../repositories/timelineRepository.js"
+import urlMetadata from "url-metadata";
+import timelineRepository from "../repositories/timelineRepository.js";
 
 export async function showPosts(req, res) {
   try {
     const posts = await timelineRepository.getPosts();
-    res.send(posts.rows);
+    if (posts.rowCount < 0) {
+      return res.sendStatus(404);
+    }
+
+    let postsList = [];
+
+    for (let i = 0; i < posts.rows.length; i++) {
+       await urlMetadata(posts.rows[i].link).then(function (metadata) {
+        let newObj={}
+        let imagePath = metadata.image;
+        const source = metadata.source;
+        if (imagePath.length === 0) {
+          metadata.image =
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Image_not_available.png/640px-Image_not_available.png";
+          let md_image = metadata.image;
+          let md_description = metadata.description;
+          let md_title = metadata.title;
+          newObj = { ...posts.rows[i], md_image,md_description,md_title};
+          postsList.push(newObj)
+          return
+        } else if (!imagePath.includes("http")) {
+          imagePath = "https://" + source + imagePath;
+          metadata.image = imagePath;
+        }
+        let md_title = metadata.title;
+        let md_image = metadata.image;
+        let md_description = metadata.description;
+        newObj = { ...posts.rows[i], md_image,md_description,md_title};
+        postsList.push(newObj)
+      });
+      
+    }
+    res.send(postsList);
   } catch (err) {
     console.log(err);
     res.sendStatus(500);
