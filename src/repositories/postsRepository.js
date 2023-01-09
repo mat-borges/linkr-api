d9af8e3c32d5b220de9ffb49214d5b59112f499b
Oscar Maia <oscarfgmaia@gmail.com>
2023-01-08 21:12:37 -0300

    feat: getPostMetadata created

--- src/repositories/postsRepository.js
+++ src/repositories/postsRepository.js
@@ -1,24 +1,33 @@
-import { connection } from '../db/db.js';
+import { connection } from "../db/db.js";
 
 async function publish(user_id, link, description) {
-  return connection.query(`INSERT INTO posts (user_id, link, description) VALUES ($1,$2,$3);`, [
-    user_id,
-    link,
-    description,
-  ]);
+  return connection.query(
+    `INSERT INTO posts (user_id, link, description) VALUES ($1,$2,$3);`,
+    [user_id, link, description]
+  );
 }
 
 async function getUserPosts(user_id) {
-  return connection.query(`SELECT * FROM posts WHERE user_id=$1 ORDER BY created_at DESC;`, [user_id]);
+  return connection.query(
+    `SELECT * FROM posts WHERE user_id=$1 ORDER BY created_at DESC;`,
+    [user_id]
+  );
 }
 
-async function deleteUserPosts(posts_id){
-  return connection.query(`DELETE FROM posts WHERE posts.id=$1;`, [posts_id])
+async function deleteUserPosts(posts_id) {
+  return connection.query(`DELETE FROM posts WHERE posts.id=$1;`, [posts_id]);
+}
+
+async function getPostMetadata(posts_id) {
+  return connection.query(`SELECT link FROM posts WHERE posts.id = $1`, [
+    posts_id,
+  ]);
 }
 const postsRepository = {
   publish,
   getUserPosts,
   deleteUserPosts,
+  getPostMetadata,
 };
 
 export default postsRepository;
--- src/routes/postsRouter.js
+++ src/routes/postsRouter.js
@@ -3,11 +3,13 @@ import { authenticateUser, verifySession } from '../middlewares/verifyAuthorizat
 import { Router } from 'express';
 import { hashtagExists } from '../middlewares/hashtagsMiddleware.js';
 import { postSchemaValidation } from '../middlewares/postsMiddleware.js';
-import { publishLink, likePost } from '../controllers/postsController.js';
+import { publishLink, likePost,getPostMetadata } from '../controllers/postsController.js';
 
 const router = Router();
 
 router.post('/posts/publish', authenticateUser, verifySession, postSchemaValidation, hashtagExists, publishLink);
 router.post('/posts/:id/like', authenticateUser, verifySession, likePost);
+router.get('/posts/:id/metadata', getPostMetadata);
 
 export default router;
+
