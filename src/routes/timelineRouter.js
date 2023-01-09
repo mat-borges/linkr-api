41912bedbdfb181d048c101ef260992bf33a239c
Mateus Borges <borgesm.work@gmail.com>
2023-01-09 15:03:58 -0300

    fix: remove duplicate variables

--- src/routes/timelineRouter.js
+++ src/routes/timelineRouter.js
@@ -1,13 +1,11 @@
 import { authenticateUser, verifySession } from '../middlewares/verifyAuthorizationMiddleware.js';
+import { deletePost, updatePost } from '../controllers/postsController.js';
 import { showPosts, showPostsOfUser } from '../controllers/timelineController.js';
 
 import { Router } from 'express';
-import { verify } from 'jsonwebtoken';
-import { deletePost, updatePost } from '../controllers/postsController.js';
-import { showPosts } from '../controllers/timelineController.js';
 import { hashtagExists } from '../middlewares/hashtagsMiddleware.js';
 import { postSchemaValidation } from '../middlewares/postsMiddleware.js';
-import { authenticateUser, verifySession } from '../middlewares/verifyAuthorizationMiddleware.js';
+import { verify } from 'jsonwebtoken';
 
 const router = Router();
 
