958c0cebff6613aab476b9a0d2635352efc64229
Mateus Borges <borgesm.work@gmail.com>
2023-01-12 19:42:39 -0300

    fix: timelineRouter router.use()

--- src/routes/timelineRouter.js
+++ src/routes/timelineRouter.js
@@ -1,22 +1,22 @@
 import { authenticateUser, verifySession } from '../middlewares/verifyAuthorizationMiddleware.js';
 import { deletePost, repostPost, updatePost } from '../controllers/postsController.js';
+import { followUser, unfollowUser } from '../controllers/followsControllers.js';
+import { followingValidation, postSchemaValidation, unfollowingValidation } from '../middlewares/postsMiddleware.js';
 import { showPosts, showPostsOfUser } from '../controllers/timelineController.js';
-import {followUser, unfollowUser} from '../controllers/followsControllers.js'
 
 import { Router } from 'express';
 import { hashtagExists } from '../middlewares/hashtagsMiddleware.js';
-import { followingValidation, unfollowingValidation, postSchemaValidation } from '../middlewares/postsMiddleware.js';
 
 const router = Router();
 
 router.use(authenticateUser, verifySession);
 
 router.get('/timeline', showPosts);
-router.delete('/timeline/:id', authenticateUser, verifySession, deletePost);
+router.delete('/timeline/:id', deletePost);
 router.put('/timeline/:id', postSchemaValidation, hashtagExists, updatePost);
 router.get('/timeline/user/:id', showPostsOfUser);
-router.post('/timeline/user/:id', authenticateUser, verifySession, followingValidation, followUser)
-router.delete('/timeline/user/:id/:userId', authenticateUser, verifySession, unfollowingValidation, unfollowUser)
-router.post('/timeline/:id', authenticateUser, verifySession, repostPost)
+router.post('/timeline/user/:id', followingValidation, followUser);
+router.delete('/timeline/user/:id/:userId', unfollowingValidation, unfollowUser);
+router.post('/timeline/:id', repostPost);
 
 export default router;
