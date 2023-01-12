3bab072dea94c38e2bb60d116b2fd3104fa755a7
Oscar Maia <oscarfgmaia@gmail.com>
2023-01-11 21:08:08 -0300

    refact: send only 10 last posts

--- src/repositories/timelineRepository.js
+++ src/repositories/timelineRepository.js
@@ -3,7 +3,7 @@ import { connection } from '../db/db.js';
 async function getPosts() {
   return connection.query(`SELECT *, posts.id as posts_id FROM posts LEFT JOIN users ON users.id=posts."user_id"
   ORDER BY posts."created_at" DESC
-  LIMIT 20;`)
+  LIMIT 10;`)
 }
 
 const timelineRepository = {
